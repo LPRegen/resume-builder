72c7a14016fa15d0e598f0f33a20137050691da6
LPRegen <manuel.escribano.051@gmail.com>
2022-06-27 08:58:46 -0300

    Modify and add new color properties.

--- src/components/theme.js
+++ src/components/theme.js
@@ -1,6 +1,9 @@
 export const colors = {
   bgPrimary: '#282a37',
   bgSecondary: '#545b69',
-  active: '#ffffff',
+  label: '#6F727C',
+  bgInput: '#3D404B',
+  primaryText: '#ffffff',
+  invalidText: '#d23333',
   selected: '#1D90F5',
 };
