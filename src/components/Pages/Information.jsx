f4de653374c7c1034dbe5d51b73cc4dacd20904f
LPRegen <manuel.escribano.051@gmail.com>
2022-06-27 09:02:37 -0300

    Add input fields

--- src/components/Pages/Information.jsx
+++ src/components/Pages/Information.jsx
@@ -5,7 +5,33 @@ export default class Information extends Component {
   render() {
     return (
       <Card>
-        <Card.Title title="Testing" />
+        <Card.Title title="Information" />
+        <Card.Form>
+          <Card.Label labelTitle="First name">
+            <Card.Input
+              inputType="text"
+              placeholder="Liam"
+              inputName="first-name"
+            />
+          </Card.Label>
+          <Card.Label labelTitle="Last name">
+            <Card.Input
+              inputType="text"
+              placeholder="Anderson"
+              inputName="last-name"
+            />
+          </Card.Label>
+          <Card.Label labelTitle="Professional title">
+            <Card.Input
+              inputType="text"
+              placeholder="Software Engineer"
+              inputName="professional-title"
+            />
+          </Card.Label>
+          <Card.Label labelTitle="Profile">
+            <Card.TextArea placeholder="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eleifend augue quis dui rhoncus molestie. Donec eget varius dolor. Praesent." />
+          </Card.Label>
+        </Card.Form>
       </Card>
     );
   }
