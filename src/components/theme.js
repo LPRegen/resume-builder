787bc5b0072a79a05bbc3402f0e3299ef77141cc
LPRegen <manuel.escribano.051@gmail.com>
2022-07-01 04:20:15 -0300

    Add color variable

--- src/components/theme.js
+++ src/components/theme.js
@@ -1,6 +1,8 @@
 export const colors = {
   bgPrimary: '#282a37',
   bgSecondary: '#545b69',
+  bgSkills: '#323644',
+  bgAddBtn: '#5A6073',
   label: '#6F727C',
   bgInput: '#3D404B',
   primaryText: '#ffffff',
