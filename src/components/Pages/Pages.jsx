f9d82ce994f5755df169a83a2967225b8ca481d3
LPRegen <manuel.escribano.051@gmail.com>
2022-07-01 02:43:11 -0300

    Add pages

--- src/components/Pages/Pages.jsx
+++ src/components/Pages/Pages.jsx
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
 import { Route, Routes } from 'react-router-dom';
+
 import Information from './Information';
+import Contact from './Contact';
+import Education from './Education';
+import Work from './Work';
+import Skills from './Skills';
 
 export default class Pages extends Component {
   render() {
     return (
       <Routes>
         <Route path="/information" element={<Information />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/education" element={<Education />} />
+        <Route path="/work" element={<Work />} />
+        <Route path="/skills" element={<Skills />} />
       </Routes>
     );
   }
