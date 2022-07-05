04b9448ff8049791d8abbb25e488072b491a2847
LPRegen <manuel.escribano.051@gmail.com>
2022-07-05 06:38:25 -0300

    Add name attribute

--- src/components/Card/TextArea.jsx
+++ src/components/Card/TextArea.jsx
@@ -5,7 +5,12 @@ import { colors } from '../theme';
 export default class TextArea extends Component {
   render() {
     return (
-      <StyledTextArea cols="25" rows="8" placeholder={this.props.placeholder} />
+      <StyledTextArea
+        cols="25"
+        rows="8"
+        placeholder={this.props.placeholder}
+        name={this.props.name}
+      />
     );
   }
 }
