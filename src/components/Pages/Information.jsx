c929542ef673f88aac3ebcf973ceec3464d6ccde
LPRegen <manuel.escribano.051@gmail.com>
2022-07-12 03:11:24 -0300

    Pass in state and event handlers as props to component

--- src/components/Pages/Information.jsx
+++ src/components/Pages/Information.jsx
@@ -3,18 +3,28 @@ import Card from '../Card';
 
 export default class Information extends Component {
   render() {
+    let information = { ...this.props.informationState };
+
     return (
       <Card>
         <Card.Title title="Information" />
         <Card.Form handleSubmit={this.props.handleSubmit}>
           <Card.Label labelTitle="First name">
-            <Card.Input inputType="text" placeholder="Liam" inputName="first" />
+            <Card.Input
+              inputType="text"
+              placeholder="Liam"
+              inputName="first"
+              value={information.first}
+              handleChange={this.props.handleChange}
+            />
           </Card.Label>
           <Card.Label labelTitle="Last name">
             <Card.Input
               inputType="text"
               placeholder="Anderson"
               inputName="last"
+              value={information.last}
+              handleChange={this.props.handleChange}
             />
           </Card.Label>
           <Card.Label labelTitle="Professional title">
@@ -22,16 +32,21 @@ export default class Information extends Component {
               inputType="text"
               placeholder="Software Engineer"
               inputName="title"
+              value={information.title}
+              handleChange={this.props.handleChange}
             />
           </Card.Label>
           <Card.Label labelTitle="Profile">
             <Card.TextArea
               name="profile"
               placeholder="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eleifend augue quis dui rhoncus molestie. Donec eget varius dolor. Praesent."
+              value={information.profile}
+              handleChange={this.props.handleChange}
             />
           </Card.Label>
           <Card.Wrapper primary>
             <Card.Button
+              primary={'true'}
               buttonType="submit"
               buttonText="Contact"
               direction="next"
