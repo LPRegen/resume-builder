33e5821f7b43c3f2d5daab0273f6d20fbf3a9916
LPRegen <manuel.escribano.051@gmail.com>
2022-07-14 15:59:20 -0300

    Add conditional styling depending on properties.

--- src/components/Card/Wrapper.jsx
+++ src/components/Card/Wrapper.jsx
@@ -4,13 +4,35 @@ import styled from 'styled-components';
 export default class Wrapper extends Component {
   render() {
     return (
-      <StyledDiv primary={this.props.primary}>{this.props.children}</StyledDiv>
+      <StyledDiv wrapperType={this.props.wrapperType}>
+        {this.props.children}
+      </StyledDiv>
     );
   }
 }
 
 const StyledDiv = styled.div`
   display: flex;
-  justify-content: space-between;
-  margin-top: ${(props) => (props.primary ? '2.5rem' : '1.5rem')};
+  justify-content: ${(props) => {
+    switch (props.wrapperType) {
+      case 'primary':
+        return 'space-between';
+      case 'terciary':
+        return 'space-evenly';
+      default:
+        return '';
+    }
+  }};
+  margin-top: ${(props) => {
+    switch (props.wrapperType) {
+      case 'primary':
+        return '2.5rem';
+      case 'secondary':
+        return '1.5rem';
+      case 'terciary':
+        return '1rem';
+      default:
+        return '';
+    }
+  }};
 `;
