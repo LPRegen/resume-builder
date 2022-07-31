cd3d6f46e047c937fdb6563f599f25c9d1fca69a
LPRegen <manuel.escribano.051@gmail.com>
2022-07-31 02:30:44 -0300

    feat: add Text Area component

--- src/components/Pages/Work.jsx
+++ src/components/Pages/Work.jsx
@@ -46,29 +46,39 @@ export default class Work extends Component {
               handleChange={this.props.handleChange}
             />
           </Card.Label>
+          <Card.Label labelTitle="Work descrition">
+            <Card.TextArea
+              name="description"
+              placeholder="Nunc consequat metus a velit cursus, id posuere metus pretium. Praesent pharetra pulvinar neque at congue."
+              value={work[selectedExperience].description}
+              handleChange={this.props.handleChange}
+            />
+          </Card.Label>
         </Card.Form>
-        <Card.Wrapper wrapperType="secondary">
-          <Card.ButtonDelete
-            deleteExperience={this.props.deleteExperience}
-            display={work.length > 2 ? 'true' : 'false'}
-          />
-        </Card.Wrapper>
-        <Card.Wrapper wrapperType="secondary">
-          {work[0] > 1 && (
-            <Card.ButtonAdd
-              direction="previous"
-              text="Previous"
-              onClick={this.props.onClick}
+        <Card.Wrapper wrapperType="terciary">
+          <Card.Wrapper wrapperType="secondary">
+            <Card.ButtonDelete
+              deleteExperience={this.props.deleteExperience}
+              display={work.length > 2 ? 'true' : 'false'}
             />
-          )}
-          {work[0] < 6 && (
-            <Card.ButtonAdd
-              primary
-              direction="next"
-              text={work[selectedExperience + 1] ? 'Next' : 'Add'}
-              onClick={this.props.onClick}
-            />
-          )}
+          </Card.Wrapper>
+          <Card.Wrapper wrapperType="secondary">
+            {work[0] > 1 && (
+              <Card.ButtonAdd
+                direction="previous"
+                text="Previous"
+                onClick={this.props.onClick}
+              />
+            )}
+            {work[0] < 6 && (
+              <Card.ButtonAdd
+                primary
+                direction="next"
+                text={work[selectedExperience + 1] ? 'Next' : 'Add'}
+                onClick={this.props.onClick}
+              />
+            )}
+          </Card.Wrapper>
         </Card.Wrapper>
         <Card.Wrapper wrapperType="primary" position="center">
           <Card.Button
